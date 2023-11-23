9fc93c0b3df6c1592f6f77c6440496626c3bd25d
a.ovchinnikova <anuta-zh@mail.ru>
2023-11-23 15:35:07 +0300

    Add ApiPlatform

--- src/Entity/Docket.php
+++ src/Entity/Docket.php
@@ -2,28 +2,53 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\OpenApi\Model\Operation as OpenApiOperation;
 use App\Repository\DocketRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: DocketRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(normalizationContext: ['groups' => 'docket:item']),
+        new GetCollection(normalizationContext: ['groups' => 'docket:list']),
+		new Post(normalizationContext: ['groups' => 'docket:item'], 
+				status: 301
+		),
+        new Put(),
+        new Delete()
+    ],
+    order: ['isDone' => 'DESC', 'name' => 'ASC'],
+    paginationEnabled: false,
+)]
 class Docket
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+	#[Groups(['docket:list', 'docket:item'])]
     private ?int $id = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
     private ?\DateTimeInterface $dateCreate = null;
 
     #[ORM\Column(length: 125)]
+	#[Groups(['docket:list', 'docket:item'])]
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
+	#[Groups(['docket:list', 'docket:item'])]
     private ?string $inform = null;
 
     #[ORM\Column]
+	#[Groups(['docket:list', 'docket:item'])]
     private ?bool $isDone = null;
 
     public function getId(): ?int
